94c2ba49da6bc4514631d24718a49e61b24f3e15
flo <flo.macicasan@gmail.com>
2016-12-08 19:31:05 +0200

    adds "Add example step 1"

---
+++ src/main/java/org/fasttrackit/util/Example.java
@@ -0,0 +1,18 @@
+package org.fasttrackit.util;
+
+/**
+ * @author flo
+ * @since 08/12/2016.
+ */
+public class Example {
+    public static void main(String[] a) {
+        System.out.println("main body");
+
+        int sum = add(2, 8);
+        System.out.println("Sum is:" + sum);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
