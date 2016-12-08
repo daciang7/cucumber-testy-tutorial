a07ba6801e369e0b835d396d12236656262f0a64
flo <flo.macicasan@gmail.com>
2016-12-08 21:24:06 +0200

    separate parity

--- src/main/java/org/fasttrackit/util/ArrayExamples.java
+++ src/main/java/org/fasttrackit/util/ArrayExamples.java
@@ -12,5 +12,40 @@ public class ArrayExamples {
         int intArr2[] = {1, 2, 3};
         int [][] matrix = new int[3][4];
         matrix[2][2] = 2;
+
+        int[] arrayInt = {11, 1, 15, 3, 7, 9, 11, 13};
+        int[] squares = new int[arrayInt.length];
+        for( int i = 0; i < arrayInt.length; i++) {
+            squares[i] = arrayInt[i] * arrayInt[i];
+        }
+        for(int square : squares) {
+            System.out.println(square);
+        }
+
+        //separate parity
+        int[] par = new int[arrayInt.length];
+        int countPar = 0, countImpar = 0;
+        int[] impar = new int[arrayInt.length];
+        for(int i = 0; i< arrayInt.length ; i++) {
+            if(arrayInt[i] % 2 == 0) {
+                //add pare
+                par[countPar] = arrayInt[i];
+                countPar++;
+            } else {
+                //add impare
+                impar[countImpar] = arrayInt[i];
+                countImpar++;
+            }
+        }
+        // print pare
+        System.out.println("Counter par" + countPar);
+        for(int i = 0 ; i< countPar; i++) {
+            System.out.print(par[i] + ",");
+        }
+        System.out.println("");
+        // print impare
+        for(int i = 0; i< countImpar ; i ++) {
+            System.out.print(impar[i] + ",");
+        }
     }
 }
