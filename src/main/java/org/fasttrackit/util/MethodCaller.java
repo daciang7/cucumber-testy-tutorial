6d3bd4deaf08a0538c582f0b70889bc404ede22f
flo <flo.macicasan@gmail.com>
2016-12-08 20:59:40 +0200

    method calls

---
+++ src/main/java/org/fasttrackit/util/MethodCaller.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.util;
+
+/**
+ * @author flo
+ * @since 08/12/2016.
+ */
+public class MethodCaller {
+
+    public static void main(String[] args) {
+        MethodExamples.sayHelloAgain();
+        MethodExamples.sayHello();
+    }
+}
---
+++ src/main/java/org/fasttrackit/util/MethodExamples.java
@@ -0,0 +1,16 @@
+package org.fasttrackit.util;
+
+/**
+ * @author flo
+ * @since 08/12/2016.
+ */
+public class MethodExamples {
+    public static void sayHelloAgain() {
+        System.out.println("Lab 3 Hello again!");
+    }
+
+    public static void sayHello() {
+        System.out.println("Lab 3 Hello!");
+    }
+
+}
