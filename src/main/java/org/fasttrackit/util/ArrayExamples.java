ff2baefde77061caa9529cbd899401d6ed4b2ec1
flo <flo.macicasan@gmail.com>
2016-12-08 21:03:14 +0200

    out of boundaries array

---
+++ src/main/java/org/fasttrackit/util/ArrayExamples.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.util;
+
+/**
+ * @author flo
+ * @since 08/12/2016.
+ */
+public class ArrayExamples {
+
+    public static void main(String[] args) {
+        int[] intArr = new int[4];
+        intArr[4] = 123;
+    }
+}
