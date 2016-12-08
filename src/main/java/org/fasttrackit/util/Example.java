83a70d10e4c9fd4904865b4459e33e1ed01ce19b
flo <flo.macicasan@gmail.com>
2016-12-08 19:34:59 +0200

    adds "Add example step 2 - Calc same file"

--- src/main/java/org/fasttrackit/util/Example.java
+++ src/main/java/org/fasttrackit/util/Example.java
@@ -8,10 +8,13 @@ public class Example {
     public static void main(String[] a) {
         System.out.println("main body");
 
-        int sum = add(2, 8);
+        int sum = Calc.add(2, 8);
         System.out.println("Sum is:" + sum);
     }
 
+}
+
+class Calc {
     public static int add(int a, int b) {
         return a + b;
     }
