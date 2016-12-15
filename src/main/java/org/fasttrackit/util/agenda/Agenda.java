0401741ece2708d72b16f277cb46334bc60fae3f
flo <flo.macicasan@gmail.com>
2016-12-15 20:38:01 +0200

    menu read from keyboard

--- src/main/java/org/fasttrackit/util/agenda/Agenda.java
+++ src/main/java/org/fasttrackit/util/agenda/Agenda.java
@@ -1,10 +1,26 @@
 package org.fasttrackit.util.agenda;
 
+import org.fasttrackit.util.Calc;
+
 /**
  * @author flo
  * @since 15/12/2016.
  */
 public class Agenda {
 
+    public static void main(String[] args) {
+
+        int option;
+        do {
+            System.out.println("1-Create");
+            System.out.print("2-UpdateByName\n");
+            System.out.print("3-DeleteByName | 4 - searchByName |");
+            System.out.println(" 5 - printAll");
+            System.out.println("9 - Exit");
+            option = Calc.readIntFromConsole();
+
+        } while(option != 9);
+
+    }
 
 }
