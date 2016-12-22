fede8d465baf71833c577e6db5d2df11dcc1ba11
flo <flo.macicasan@gmail.com>
2016-12-22 20:50:13 +0200

    adds Object methods override

--- src/main/java/org/fasttrackit/util/quizz/Vehicle.java
+++ src/main/java/org/fasttrackit/util/quizz/Vehicle.java
@@ -12,9 +12,21 @@ public abstract class Vehicle {
         System.out.println("Vehicle Moves");
     }
 
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
     public static void main(String[] args) {
-        Vehicle v = new Car();
-        Car c = new Car();
+        Vehicle v = new Car(3);
+        Car c = new Car(3);
+        Car b = new Car(4);
+        System.out.println(c.toString());
+        System.out.println(c == v);
+        System.out.println(c.equals(v));
+        System.out.println(c.equals(b));
+        System.out.println(c.equals("cucurigu"));
+
 
 //        v.moves();
         c.moves();
@@ -24,10 +36,25 @@ public abstract class Vehicle {
 
 class Car extends Vehicle {
 
+    private int id;
+
+    public Car(int id) {
+        this.id = id;
+    }
+
     public String honk() {
         this.moves();
         super.moves();
         return "Tiiiitt";
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(! (other instanceof Car)) {
+           return false;
+        }
+        Car car = (Car)other;
+        return car.id == this.id;
     }
 
     @Override
