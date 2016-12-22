a35ef9413407314cd960b8a8b6dec61ba259271d
flo <flo.macicasan@gmail.com>
2016-12-22 19:42:59 +0200

    adds q2 from quiz

---
+++ src/main/java/org/fasttrackit/util/quizz/Q2.java
@@ -0,0 +1,22 @@
+package org.fasttrackit.util.quizz;
+
+/**
+ * @author flo
+ * @since 22/12/2016.
+ */
+public class Q2 {
+
+
+    public static void main(String[] abracadabra) {
+        for (int x = 1; x <= 3; x++) {
+            for (int y = 1; y <= x; y++) {
+                System.out.print(y + " ");
+            }
+        }
+        System.out.println(abracadabra);
+        int a = Integer.valueOf("4");
+    }
+
+    int answer = 5;
+
+}
