66e25ed06590e12e9925460ed8676c444d9ccd93
flo <flo.macicasan@gmail.com>
2016-12-24 11:05:53 +0200

    adds custom exception

---
+++ src/main/java/org/fasttrackit/util/quizz/TextException.java
@@ -0,0 +1,11 @@
+package org.fasttrackit.util.quizz;
+
+/**
+ * @author flo
+ * @since 22/12/2016.
+ */
+public class TextException extends Exception {
+    public TextException(String message) {
+        super(message);
+    }
+}
