9a531db9a6e99f9843bf53fcb6abb9adc31ee938
Dacian <daciang7@gmail.com>
2017-01-12 22:00:49 +0200

    Primul test

---
+++ src/test/java/org/fasttrackit/Automation/LoginTest.java
@@ -0,0 +1,40 @@
+package org.fasttrackit.Automation;
+
+import org.apache.xpath.SourceTree;
+import org.fasttrackit.util.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Dacian on 1/12/2017.
+ */
+public class LoginTest extends TestBase{
+    @Test
+    public void validLoginTest(){
+        System.out.println("ready");
+        driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
+
+        WebElement emailField = driver.findElement(By.id("email"));
+        WebElement passField =driver.findElement(By.name("password"));
+        WebElement loginBtn=driver.findElement(By.className("login-btn"));
+
+        emailField.sendKeys("eu@fast.com");
+        passField.sendKeys("eu.pass");
+        loginBtn.click();
+        try {
+            WebElement logoutBtn = driver.findElement(By.linkText("Logout"));
+            logoutBtn.click();
+        } catch(NoSuchElementException er){
+            Assert.fail("Could not login.Logout button was not present");
+    }
+
+}
+
+
+
+
+    }
+
